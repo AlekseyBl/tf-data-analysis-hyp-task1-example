850defaca2d23f36181a49f015b9077a76113d14
AlekseyBl <130070481+AlekseyBl@users.noreply.github.com>
2023-04-13 20:19:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,7 +6,7 @@ from statsmodels.stats.proportion import proportions_ztest
 chat_id = 6130040059 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
-    alpha = 0.09
+    alpha = 0.07
     _, p_value = proportions_ztest(
         [x_success, y_success], [x_cnt, y_cnt], alternative="smaller"
     )
